f13e06de9a03ec8567367add8d03c63d831df49c
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2026-02-21 10:10:19 +0200

    Add Language.js for language loading functionality

---
+++ Language.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+require('./settings');
+
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./language/' + global.LANG + '.json')) {
+
+    console.log(
+        chalk.green.bold('Loading ' + global.LANG + ' language...')
+    );
+
+    var json = JSON.parse(
+        fs.readFileSync('./language/' + global.LANG + '.json')
+    );
+
+} else {
+
+    console.log(
+        chalk.red.bold(
+            'You entered an invalid language. English language was chosen.'
+        )
+    );
+
+    var json = JSON.parse(
+        fs.readFileSync('./language/EN.json')
+    );
+}
+
+function getString(key) {
+    return json.STRINGS[key];
+}
+
+module.exports = {
+    language: json,
+    getString: getString
+};
